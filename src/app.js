79041c2a797be767f9627bffc93cdec0663fcd30
George Zhu <georgezhu211@gmail.com>
2026-05-05 16:12:10 -0500

    feat: add base layout

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const layouts = require("express-ejs-layouts");
+
 const path = require("node:path");
 
 const app = express();
@@ -6,6 +8,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use(layouts);
+
+app.get("/", (req, res) => res.render("home"));
 
 app.listen(3000);
