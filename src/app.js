a4f48bd773f44e4e8e8f5f54a1147a85a2a2db8b
George Zhu <georgezhu211@gmail.com>
2026-05-05 15:42:09 -0500

    chore: set up ejs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const path = require("node:path");
 
 const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
