dcf1864b39b044c2ebbb217b7a1a349ff0f5c696
George Zhu <georgezhu211@gmail.com>
2026-05-07 00:57:52 -0500

    feat: add database seed script

---
+++ src/db/seed.js
@@ -0,0 +1,69 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+
+const SQL = `
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS session;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+  username TEXT UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL ,
+
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+
+  is_member BOOLEAN NOT NULL DEFAULT FALSE,
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id INTEGER,
+
+  title TEXT NOT NULL,
+  content TEXT NOT NULL,
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+  CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.DB_URL,
+  });
+  await client.connect();
+  await client.query("BEGIN");
+  try {
+    await client.query(SQL);
+    await client.query("COMMIT");
+  } catch (e) {
+    await client.query("ROLLBACK");
+    throw e;
+  } finally {
+    await client.end();
+  }
+  console.log("done");
+}
+
+main();
